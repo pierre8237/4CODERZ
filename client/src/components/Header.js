19ef05ef26ea25ece9b51934aa4701cea0503625
pierre8237 <34560968+pierre8237@users.noreply.github.com>
2020-10-12 21:07:56 -0400

    mathews files

---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Header.css";
+import cheese from "./Images/cheese-plate.jpg";
+
+
+
+function Header() {
+  return (
+    
+      <img src={cheese} alt="Wine and Chease" width="1888 px" />
+    
+  );
+}
+
+
+
+export default Header;
