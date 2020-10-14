53f7878465e6b393911f7c90e1ab5a943b27b55e
Mathew Poovathany <poovathanymathew@gmail.com>
2020-10-14 17:10:19 -0400

    Developer Bio is added at the bottom

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "../styles/Section1.css";
+import mathew from "./Images/team4.jpeg"
+import Colin from "./Images/dummy.jpg";
+import Aimee from "./Images/dummy.jpg";
+import Pierre from "./Images/dummy.jpg";
 
 
 
@@ -35,6 +39,64 @@ function Section1() {
       </div>
    </div>
   
+  
+<div class="row">
+<div class="column">
+    <div class="card">
+      <img src={Aimee} alt="Aimee"  />
+      <div class="container">
+        <h2>Aimee Millard</h2>
+        <p class="title">Developer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><a href="https://github.com/">GitHub </a></p>
+        
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src={mathew} alt="Mathew"  />
+      <div class="container">
+        <h2>Mathew Poovathany</h2>
+        <p class="title">Developer</p>
+        <p>Worked on UI. Which includes HTML and CSS coding in react app.</p>
+        <p>Poovathanymathew@gmail.com</p>
+        <p><a href="https://github.com/mathewpoovathany">GitHub </a></p>
+        
+      </div>
+    </div>
+  </div>
+ 
+   <div class="column">
+    <div class="card">
+      <img src={Pierre} alt="Pierre" />
+      <div class="container">
+        <h2>Pierre Calmles</h2>
+        <p class="title">Developer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><a href="https://github.com/">GitHub </a></p>
+        
+      </div>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <img src={Colin} alt="Colin"  />
+      <div class="container">
+        <h2>Colin</h2>
+        <p class="title">Developer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><a href="https://github.com/">GitHub </a></p>
+      </div>
+    </div>
+  </div>
+</div>
+
   </div>   
     
   );
